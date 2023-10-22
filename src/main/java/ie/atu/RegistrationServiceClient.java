8a97b5cb674eb7677982fcac2c3c4bed1cd29086
G00405094 <G00405094@atu.ie>
2023-10-22 22:32:53 +0100

    fixed bugs

--- src/main/java/ie/atu/RegistrationServiceClient.java
+++ src/main/java/ie/atu/RegistrationServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "confirmation-service", url = "http://localhost:8082")
+@FeignClient(name = "confirmation-service", url = "http://localhost:8080")
 public interface RegistrationServiceClient {
     @PostMapping("/confirm")
     String someDetails(@RequestBody UserDetails userDetails);
